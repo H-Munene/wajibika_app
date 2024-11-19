356f79fb9a4dadd98ec67e9da61ae8279bcb6097
Hezekiah Munene <hezemunene01@gmail.com>
2024-11-19 18:30:39 +0300

    Created the login,register button widget class

---
+++ lib/widgets/loginregisterbtn.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:wajibika_app/utils/globals.dart' as globals;
+
+class LoginRegisterButton extends StatelessWidget {
+  final VoidCallback clickAction;
+  final String buttonText;
+  final Color btnColor;
+
+  const LoginRegisterButton(
+      {super.key,
+      required this.clickAction,
+      required this.btnColor,
+      required this.buttonText});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: globals.buttonHeight,
+      width: globals.buttonWidth,
+      child: ElevatedButton(
+        onPressed: clickAction,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: btnColor,
+        ),
+        child: Text(
+          buttonText,
+          style: const TextStyle(color: globals.buttonTextColor),
+        ),
+      ),
+    );
+  }
+}
