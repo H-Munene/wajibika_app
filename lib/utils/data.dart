340e12aa79d28c5b0b53d5ea3bab618b98c2eb68
Hezekiah Munene <hezemunene01@gmail.com>
2024-11-25 09:14:38 +0300

    created a counterup widget. Added JSON data to data.dart

--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -1,1 +1,533 @@
-const List<String> financialYears = ['f2023/2024', 'f2022/2023', 'f2021/2022'];
+var stalledMDAProjects = {
+  "totalContractAmount(Kshs.)" : "3,716,569,830",
+  "totalAmountPaid (Kshs.)" : "3,100,222,841",
+  "Departments" : [
+  {
+    //interior services
+    "stateDepartment": "Interior and Citizen Services",
+    "Contract Amount (Kshs.)": "995,275,385",
+    "totalAmountPaid": "156,968,272",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "764,444,795",
+        "amount paid": "36,245,978",
+        "projects": [
+          {
+            "name": "Construction of DCC office block",
+            "location": "Tigania, Meru",
+            "status": "Stalled"
+          },
+          {
+            "name": "Construction of Tigania East Sub-County office block",
+            "location": "Muriri, Meru",
+            "status": "Stalled"
+          },
+          {
+            "name":
+                "Construction of Magunga District Headquarters Suba South Sub-County",
+            "location": "Homa-bay",
+            "status": "Stalled"
+          }
+        ]
+      },
+      "group 2": {
+        "amount allocated": "230,830,590",
+        "amount paid": "120,722,294",
+        "projects": [
+          {
+            "name": "Delayed completion of Mwea West Sub-County Headquarters",
+            "location": "Kirinyaga",
+            "status": "Stalled"
+          },
+          {
+            "name":
+                "Construction of 100 PAX hostel block at Kenya School of Adventure and Leadership",
+            "location": "Meru",
+            "status": "Stalled"
+          }
+        ]
+      }
+    }
+  }, //end of interior
+  {
+    //water and sanitation
+    "stateDepartment": "Water and Sanitation",
+    "Contract Amount (Kshs.)": "2,279,500,662",
+    "totalAmountPaid": "449,495,494",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "9,430,012",
+        "amount paid": "5,813,700",
+        "projects": [
+          {
+            "name": "Borehole project at Njiiri School",
+            "location": "Murang'a",
+            "status": "Stalled"
+          },
+        ]
+      },
+      "group 2": {
+        "amount allocated": "2,270,070,650",
+        "amount paid": "443,681,794",
+        "projects": [
+          {
+            "name":
+                "Works and services at Upper Kerio Valley and Upper Ewaso Nyiro North River water sheds",
+            "location": "Inter-County",
+            "status": "Stalled"
+          },
+          {
+            "name":
+                "Borehole drilling at Muthuini Primary School in Kieni Constituency",
+            "location": "Nyeri",
+            "status": "Stalled"
+          },
+          {
+            "name": "Kanyenyaini Irrigation Project,",
+            "location": "Murang'a",
+            "status": "Stalled"
+          },
+          {
+            "name": "Sagana River Restoration Project",
+            "location": "Nyeri",
+            "status": "Stalled"
+          },
+          {
+            "name":
+                "Delayed completion of Maragua Bulk Water Supply and Irrigation Project",
+            "location": "Nyeri",
+            "status": "Stalled"
+          }
+        ]
+      }
+    }
+  },
+  {
+    //Foreign Affairs
+    "stateDepartment": "Foreign Affairs",
+    "Contract Amount (Kshs.)": "418,865,923",
+    "totalAmountPaid": "415,904,556",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "418,865,923",
+        "amount paid": "415,904,556",
+        "projects": [
+          {
+            "name":
+                "Stalled project of chancery building and High Commissioners residence in Pakistan",
+            "location": "Pakistan",
+            "status": "Stalled"
+          },
+        ]
+      },
+    }
+  },
+  {
+    //Livestock
+    "stateDepartment": "Livestock",
+    "Contract Amount (Kshs.)": "8,863,275",
+    "totalAmountPaid": "undisclosed",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "418,865,923",
+        "amount paid": "415,904,556",
+        "projects": [
+          {
+            "name":
+                "Boundary wall, entrance gate, driveway, landscape and toilet block at AHITI Nyahururu",
+            "location": "Laikipia",
+            "status": "Stalled"
+          },
+        ]
+      },
+    }
+  },
+  {
+    //Devolution
+    "stateDepartment": "Devolution",
+    "Contract Amount (Kshs.)": "890,829,345",
+    "totalAmountPaid": "250,000,000",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "890,829,345",
+        "amount paid": "250,000,000",
+        "projects": [
+          {
+            "name": "Africities Convention Centre Project",
+            "location": "Kisumu",
+            "status": "Stalled"
+          },
+        ]
+      },
+    }
+  },
+  {
+    //NPS
+    "stateDepartment": "National Police Service",
+    "Contract Amount (Kshs.)": "*105,365,412", //Undisclosed for Embakasi A hence *
+    "totalAmountPaid": "*38,855,887", //undisclosed for Laisamis hence *
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "105,365,412",
+        "amount paid": "*38,855,887",
+        "projects": [
+          {
+            "name": "Stalled construction works at Laisamis Police Station",
+            "location": "Marsabit",
+            "status": "Stalled"
+          },
+          {
+            "name": "Stalled projects at Embakasi A Campus",
+            "location": "Nairobi",
+            "status": "Stalled"
+          },
+        ]
+      },
+    }
+  },
+  {
+    //Sports
+    "stateDepartment": "State Department for Sports Development",
+    "Contract Amount (Kshs.)": "345,766,644",
+    "totalAmountPaid": "undisclosed",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "345,766,644",
+        "amount paid": "undisclosed",
+        "projects": [
+          {
+            "name": "Stalled Phase 2 construction at Kirigiti Stadium",
+            "location": "Kiambu",
+            "status": "Stalled"
+          },
+        ]
+      },
+    }
+  },
+  {
+    //Correctional Services
+    "stateDepartment": "Correctional Services",
+    "Contract Amount (Kshs.)": "28,812,175",
+    "totalAmountPaid": "12,846,470",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "28,812,175",
+        "amount paid": "12,846,470",
+        "projects": [
+          {
+            "name": "Stalled projects at Nyandarua and Machakos Prisons",
+            "location": "Nyandarua, Machakos",
+            "status": "Stalled"
+          },
+        ]
+      },
+    }
+  },
+  {
+    //Housing and Urban Development
+    "stateDepartment": "Housing and Urban Development",
+    "Contract Amount (Kshs.)": "64,000,000",
+    "totalAmountPaid": "35,000,000",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "64,000,000",
+        "amount paid": "35,000,000",
+        "projects": [
+          {
+            "name":
+                "Stalled projects under Economic Stimulus Project (ESP) - fresh produce markets project",
+            "location": "",
+            "status": "Stalled"
+          },
+        ]
+      },
+    }
+  },
+  {
+    //Labour and Skills Development
+    "stateDepartment": "Labour and Skill Development",
+    "Contract Amount (Kshs.)": "556,763,065",
+    "totalAmountPaid": "339,314,478",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "556,763,065",
+        "amount paid": "339,314,478",
+        "projects": [
+          {
+            "name":
+                "Stalled construction of National Employment Promotion Centre and Research Institute Office Complex ",
+            "location": "Kiambu",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    //Technical, Vocational Education and Training
+    "stateDepartment": "Technical, Vocational Education and Training",
+    "Contract Amount (Kshs.)": "48,743,504",
+    "totalAmountPaid": "26,700,000",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "48,743,504",
+        "amount paid": "26,700,000",
+        "projects": [
+          {
+            "name":
+                "Construction of collapsed building at Chepareria Technical Training Institute ",
+            "location": "West Pokot",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // TSC
+    "stateDepartment": "Teachers Service Commission",
+    "Contract Amount (Kshs.)": "122,563,447",
+    "totalAmountPaid": "undisclosed",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "122,563,447",
+        "amount paid": "undisclosed",
+        "projects": [
+          {
+            "name":
+                "Delayed completion of Machakos County offices at 33% completion levels",
+            "location": "Machakos",
+            "status": "Stalled"
+          },
+          {
+            "name":
+                "Delayed completion of Kilifi County offices at 40% completion levels",
+            "location": "Kilifi",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // Crop Development
+    "stateDepartment": "Crop Development",
+    "Contract Amount (Kshs.)": "178,581,150",
+    "totalAmountPaid": "24,358,930",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "178,581,150",
+        "amount paid": "24,358,930",
+        "projects": [
+          {
+            "name": "Construction of Laare Modern Miraa Market Shed,",
+            "location": "Meru",
+            "status": "Stalled"
+          },
+          {
+            "name": "Mwendi-Urithi Lailuba grain drying and storage facility",
+            "location": "Meru",
+            "status": "Stalled"
+          },
+          {
+            "name": " Mbeu Muungano CBO drying and storage facility",
+            "location": "Meru",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // State Department for Petroleum
+    "stateDepartment": "Petroleum",
+    "Contract Amount (Kshs.)": "76,500,000",
+    "totalAmountPaid": "undisclosed",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "122,563,447",
+        "amount paid": "undisclosed",
+        "projects": [
+          {
+            "name": "Delayed implementation of Mwananchi Gas Project",
+            "location": "Inter-County",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // State Department for Public Works
+    "stateDepartment": "Public Works",
+    "Contract Amount (Kshs.)": "987,189,903",
+    "totalAmountPaid": "579,635,580",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "987,189,903",
+        "amount paid": "579,635,580",
+        "projects": [
+          {
+            "name":
+                "Delayed completion of six projects including footbridges and County Headquarters",
+            "location": "",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // State Department for Public Works
+    "stateDepartment": "The Judiciary",
+    "Contract Amount (Kshs.)": "826,329,820",
+    "totalAmountPaid": "undisclosed",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "826,329,820",
+        "amount paid": "undisclosed",
+        "projects": [
+          {
+            "name":
+                "Delayed completion of fifteen (15) projects at various court stations for over eight (8) years with 50% to 97% work done",
+            "location": "Inter-County",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // Parliamentary Joint Services
+    "stateDepartment": "Parliamentary Joint Services",
+    "Contract Amount (Kshs.)": "undisclosed",
+    "totalAmountPaid": "752,448,996",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "752,448,996",
+        "amount paid": "undisclosed",
+        "projects": [
+          {
+            "name":
+                "Delayed completion of multi-storey office block, started on 1 July, 2014 and completion time extended to 30 November, 2021",
+            "location": "Nairobi",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // Energy
+    "stateDepartment": "Energy",
+    "Contract Amount (Kshs.)": "3,055,985,900",
+    "totalAmountPaid": "undisclosed",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "3,055,985,900",
+        "amount paid": "undisclosed",
+        "projects": [
+          {
+            "name":
+                "Delayed implementation of the Green Climate Fund Project Phase I and II. GoK did not provide co-funding",
+            "location": "Inter-County",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // State Department for Medical Services
+    "stateDepartment": "Medical Services",
+    "Contract Amount (Kshs.)":
+        "*2,280,000,000", //*due to undisclosed KNH BurnsCentre
+    "totalAmountPaid": "*131,647,317", //*due to undisclosed Kisii Cancer Center
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "2,280,000,000",
+        "amount paid": "undisclosed",
+        "projects": [
+          {
+            "name": "Construction of Kisii Cancer Centre ",
+            "location": "Kisii",
+            "status": "Stalled"
+          },
+        ]
+      },
+      "group 2": {
+        "amount allocated": "undisclosed",
+        "amount paid": "131,647,317",
+        "projects": [
+          {
+            "name":
+                "Construction of Paediatric Emergency Centre and Burns Management Centre at Kenyatta National Hospital",
+            "location": "Nairobi",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // Independent Electoral and Boundaries Commission
+    "stateDepartment": "Independent Electoral and Boundaries Commission",
+    "Contract Amount (Kshs.)": "293,504,959",
+    "totalAmountPaid": "229,929,888",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "293,504,959",
+        "amount paid": "229,929,888",
+        "projects": [
+          {
+            "name":
+                "Delayed completion of construction and renovation of six (6) warehouses in six (6) Counties despite lapse of the contract periods",
+            "location": "Inter-County",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+  {
+    // State Department of Transport
+    "stateDepartment": "Transport",
+    "Contract Amount (Kshs.)": "153,129,261",
+    "totalAmountPaid": "137,044,745",
+    "asOf": "30 June, 2023",
+    "projects groups": {
+      "group 1": {
+        "amount allocated": "153,129,261",
+        "amount paid": "137,044,745",
+        "projects": [
+          {
+            "name":
+                "Delayed completion of Transport Data Centre contract awarded in 2014",
+            "location": "Inter-County",
+            "status": "Stalled"
+          },
+        ]
+      },
+    },
+  },
+]};
