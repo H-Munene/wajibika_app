baa9a8d1227b2bda260a05460f2f3a58f66aba17
Hezekiah Munene <hezemunene01@gmail.com>
2024-11-26 19:59:39 +0300

    added data table

--- lib/widgets/state_dept.dart
+++ lib/widgets/state_dept.dart
@@ -11,37 +11,94 @@ class StateDepartentItem extends StatefulWidget {
 }
 
 class _StateDepartentItemState extends State<StateDepartentItem> {
+  bool _customIcon = false;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(bottom: 10, left: 10),
+      padding: const EdgeInsets.only(
+        bottom: 10,
+      ),
       child: Column(
         children: [
-          Row(mainAxisAlignment: MainAxisAlignment.start, children: [ Text('Roads', style: TextStyle(fontWeight: FontWeight.bold, fontSize: subtitleTextFontSize),)]),
-          //contract amount
-          Row(
-            children: [
-              Text('Contract amount: '),
-              CounterUpWidget(end: 180000000, color: Colors.green),
-            ],
+          Card(
+            child: ExpansionTile(
+              title: const Text(
+                'Interior Services',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: subtitleTextFontSize),
+              ),
+              shape: const Border(),
+              trailing: Icon(
+                  _customIcon ? Icons.arrow_drop_up : Icons.arrow_drop_down),
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Column(
+                    children: [
+                      //contract amount
+                      const Row(
+                        children: [
+                          Text('Contract amount: '),
+                          CounterUpWidget(end: 180000000, color: Colors.green),
+                        ],
+                      ),
+                      //amount paid
+                      const Row(
+                        children: [
+                          Text('Amount Paid: '),
+                          CounterUpWidget(end: 20234000, color: Colors.red),
+                        ],
+                      ),
+                      //status , pulse animation
+                      Row(
+                        children: [
+                          const Text('Status: '),
+                          SizedBox(
+                              height: 30,
+                              width: 30,
+                              child: Lottie.asset(
+                                  'assets/animations/red_pulse.json')),
+                          const Text('Stalled'),
+                        ],
+                      ),
+                      DataTable(columns: const [
+                        //Project(s)
+                        DataColumn(label: Text('Project(s)')),
+                        //Location
+                        DataColumn(label: Text('Location')),
+                        //amount allocated
+                        DataColumn(label: Text('amount allocated(Kshs.)')),
+                        //amount paid
+                        DataColumn(label: Text('amount paid(Kshs.)')),
+                      ], rows: const [
+                        DataRow(cells: [
+                          DataCell(Text('Borehole project at Njiiri School')),
+                          DataCell(Text("Murang'a")),
+                          DataCell(Text('9,430,012')),
+                          DataCell(Text('5,813,700')),
+                        ]),
+                      
+                        DataRow(cells: [
+                          DataCell(Text('Borehole project at Njiiri School')),
+                          DataCell(Text("Murang'a")),
+                          DataCell(Text('')),
+                          DataCell(Text('5,813,700')),
+                        ]),
+                      ])
+                    ],
+                  ),
+                )
+              ],
+              onExpansionChanged: (bool expanded) {
+                setState(() {
+                  _customIcon = expanded;
+                });
+              },
+            ),
           ),
-          //amount paid
-          Row(
-            children: [
-              Text('Amount Paid: '),
-              CounterUpWidget(end: 20234000, color: Colors.red),
-            ],
-          ),
-          Row(
-            children: [
-              Text('Status: '),
-              SizedBox(
-                  height: 30,
-                  width: 30,
-                  child: Lottie.asset('assets/animations/red_pulse.json')),
-                  const Text('Stalled')
-            ],
-          )
+          
         ],
       ),
     );
