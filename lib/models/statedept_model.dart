cdf37487f7fd99475ff4fa3b6cf8378d6fb7ccc8
Hezekiah Munene <hezemunene01@gmail.com>
2024-11-27 13:33:18 +0300

    Chore: created the statedepartment model

--- lib/models/statedept_model.dart
+++ lib/models/statedept_model.dart
@@ -1,3 +1,30 @@
-class StateDepartment{
-  
+class StateDepartment {
+  final String stateDepartmentName;
+  final String cumulativeContractAmounts;
+  final String cumulativeAmountPaid;
+  final Map<String, List> projects;
+
+  StateDepartment(
+      {required this.stateDepartmentName,
+      required this.cumulativeContractAmounts,
+      required this.cumulativeAmountPaid,
+      required this.projects});
+
+  // factory StateDepartment.fromJson(Map<String, dynamic> json) {
+  // return StateDepartment(
+  //   stateDepartmentName : json['stateDepartment'],
+  //   cumulative_contract_amounts:  : json['cumulative Contract Amounts (Kshs.)'],
+  //   cumulative_amountPaid :json['cumulative AmountPaid'],
+  //   projects : json['projects'],
+  // );
+//}
+  factory StateDepartment.fromJson(Map<String, dynamic> json) {
+    return StateDepartment(
+      stateDepartmentName: json['stateDepartment'],
+      cumulativeContractAmounts:
+          json['cumulative Contract Amounts (Kshs.)'].toString(),
+      cumulativeAmountPaid: json['cumulative AmountPaid'].toString(),
+      projects: json['projects'],
+    );
+  }
 }
