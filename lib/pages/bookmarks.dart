f6bc3c5df2956f2048d29d5419e4b84624b24770
Hezekiah Munene <hezemunene01@gmail.com>
2024-11-27 15:15:58 +0300

    Refactored bookmarks

--- lib/pages/bookmarks.dart
+++ lib/pages/bookmarks.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:Wajibika/models/statedept_model.dart';
 import 'package:Wajibika/utils/globals.dart';
 import 'package:Wajibika/widgets/countup.dart';
@@ -12,46 +13,39 @@ class BookMarksPage extends StatefulWidget {
   @override
   State<BookMarksPage> createState() => _BookMarksPageState();
 }
-
-// Fetch JSON
-Future<String> _loadJson() async {
-  return await rootBundle.loadString('assets/json/sample.json');
-}
+ //fetch json
+  Future<String> _loadJson() async{
+    return await rootBundle.loadString('assets/json/sample.json');
+  }
 
 class _BookMarksPageState extends State<BookMarksPage> {
-  // Variable to store state department data
+  //variable to store list
   bool _customIcon = false;
-  late StateDepartment stateDepartment;
-  bool _isDataLoaded = false;
 
-  Future loadData() async {
+  Future loadData() async{
     String jsonString = await _loadJson();
     final jsonResponse = json.decode(jsonString);
 
-    setState(() {
-      stateDepartment = StateDepartment.fromJson(jsonResponse);
-      _isDataLoaded = true;
-    });
-
+    StateDepartment stateDepartment = StateDepartment.fromJson(jsonResponse);
     print("${stateDepartment.stateDepartmentName}, ${stateDepartment.cumulativeContractAmounts}, ${stateDepartment.projects[0].name}");
   }
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
     loadData();
   }
+ 
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 10),
+      padding: const EdgeInsets.only(
+        bottom: 10,
+      ),
       child: Column(
-        children: [
-          _isDataLoaded
-              ? Text(stateDepartment.stateDepartmentName)
-              : CircularProgressIndicator(),
-        ],
+        
       ),
     );
   }
